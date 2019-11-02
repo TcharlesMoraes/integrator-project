40161a4f96f05561aa2450d90872417d6c981668
Luiz Gustavo <gustavoweberthums@hotmail.com>
2019-11-01 22:10:19 -0300

    initial commit de novo :(

---
+++ src/java/controller/FileUpload.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.Serializable;
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+
+@ManagedBean
+@Named("file")
+@RequestScoped
+public class FileUpload implements Serializable{
+    private UploadedFile file;
+ 
+    public String submeterArquivo(){
+        return "fileUpload?faces-redirect=true";
+    }
+    
+    public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+     
+    public void upload() {
+        if(file != null) {
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }else{
+            System.err.println("Errou");
+        }
+    }
+     
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }   
+}
