8e1b3e992ef35faa8f0764b28dc3d4751ca57812
luiz.weber <luiz.weber@DESKTOP-FLADUK1.VST.local>
2019-11-08 18:13:35 -0200

    Persistence AND models

--- src/java/model/Dataset.java
+++ src/java/model/Dataset.java
@@ -1,6 +1,63 @@
 package model;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
 
-public class Dataset {
+@Entity
+public class Dataset implements Serializable {
+    
+    @Id
+    private long id;
+    private String caminho;
+    private String datasetName;
+  
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date adicionadoEm;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataDataset;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCaminho() {
+        return caminho;
+    }
+
+    public void setCaminho(String caminho) {
+        this.caminho = caminho;
+    }
+
+    public String getDatasetName() {
+        return datasetName;
+    }
+
+    public void setDatasetName(String datasetName) {
+        this.datasetName = datasetName;
+    }
+
+    public Date getAdicionadoEm() {
+        return adicionadoEm;
+    }
+
+    public void setAdicionadoEm(Date adicionadoEm) {
+        this.adicionadoEm = adicionadoEm;
+    }
+
+    public Date getDataDataset() {
+        return dataDataset;
+    }
+
+    public void setDataDataset(Date dataDataset) {
+        this.dataDataset = dataDataset;
+    }
+    
     
 }
